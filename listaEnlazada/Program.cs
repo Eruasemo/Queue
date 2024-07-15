68fc846016a4b6b53927bd23fe7c36f1ee283d48
MANUEL BALDERAS VICTORIA <manuel.balderas@edu.uag.mx>
2024-07-15 17:06:51 -0600

    Agregar archivos de proyecto.

---
+++ listaEnlazada/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace listaEnlazada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nodo test = new Nodo("Hola");
+
+            //test.Dato = "Hola, que tal.";
+            //test.Siguiente = test;
+
+            //Console.WriteLine(test.Siguiente.Dato);
+
+            Cola miCola = new Cola("Mi Cola");
+
+            miCola.Encolar("Manuel");
+            miCola.Encolar("Isis");
+            miCola.Encolar("Joel");
+            Console.WriteLine(miCola.Desencolar());
+            miCola.Encolar("Melanie");
+            miCola.Encolar("Valeria");
+            miCola.Encolar("Maximilano");
+            miCola.Encolar("Barbie");
+
+            while(!miCola.estaVacia())
+            {
+               Console.WriteLine(miCola.Desencolar());
+            }
+            
+
+        }
+    }
+}
